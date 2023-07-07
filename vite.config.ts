3994638b2bab28ab7b90ce952a1a270168b9e6e7
j0131n <dev.jcantoria@gmail.com>
2023-07-08 00:40:38 +0800

    setup config, aliases, @types

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
+// dont forget to install @types/node for vue-ts
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
+  }
 })
